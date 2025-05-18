e92cbad97cccda862a8052c7d04b7eb9ad64d4d5
hannah-ric <hannah.ricci@travelandleisure.com>
2025-05-17 20:46:33 -0400

    star

--- supabase/functions/_shared/index.ts
+++ supabase/functions/_shared/index.ts
@@ -1,4 +1,8 @@
 // Main index file for shared modules
+// In your barrel file
 export * from "./cors";
 export * from "./error-handler";
 export * from "./stripe-types";
+
+// Add this line to fix the error:
+export { default } from "./cors";
